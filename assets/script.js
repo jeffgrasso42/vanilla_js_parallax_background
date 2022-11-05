7c7d441ee3e5df08b8823d8c5f2ad89868e6eb56
JAG <jagrasso42@gmail.com>
2022-11-05 16:34:58 -0400

    Added images folder and png files for each of the 5 background layers

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// DEPENDENCIES
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+// GLOBAL VARIABLES
+const CANVAS_WIDTH = (canvas.width = 800);
+const CANVAS_HEIGHT = (canvas.height = 700);
+
+let gameSpeed = 5;
